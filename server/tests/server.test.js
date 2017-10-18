e49ab0ade09388044b5386dfb30285b6d7c03b94
mkolenski <kolenskim@gmail.com>
2017-10-18 11:33:30 -0400

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,4 +100,45 @@ describe('GET /todos/:id', () => {
     .expect(404)
     .end(done)
   })
-})
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res) => {
+      if (err) {
+        return done(err);
+      }
+
+      // query db using findById toNotExist.
+      Todo.findById(hexId).then((todo) => {
+        // expect(null).toNotExist();  // pass todo arg from success handler
+        expect(todo).toNotExist();
+        // call done
+        done();
+      }).catch((e) => done(e));
+    })
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    // make sure you get a 404
+    request(app)
+    .delete(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done)
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+    .get(`/todos/123`)
+    .expect(404)
+    .end(done)
+  });
+});
