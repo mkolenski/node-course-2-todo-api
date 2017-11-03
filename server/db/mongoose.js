bba07cbf140ab1fe0504f3ae882a5ebcf7a2d2b6
mkolenski <kolenskim@gmail.com>
2017-11-03 17:44:43 -0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,10 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_lnhpb44m:esdvndfl1jk6iofov2enmjt1mt@ds131480.mlab.com:31480/heroku_lnhpb44m' || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+process.env.NODE_ENV
 
 //
